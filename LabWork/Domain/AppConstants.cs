ce569f72343ca08f0b4457456e3570b56ff5783f
Ilya Pupyshev <pupischev.ilja@gmail.com>
2024-09-25 13:01:36 +0300

    added new constants, change FigureParameters.cs

--- LabWork/Domain/AppConstants.cs
+++ LabWork/Domain/AppConstants.cs
@@ -12,19 +12,23 @@
 
 
         // Place parameters
-        public static uint PlaceWidth = 50;
-        public static uint PlaceHeight = 50;
-        public static uint PlaceThickness = 5;
+        public static uint PlaceWidth = 30;
+        public static uint PlaceHeight = 30;
+        public static uint PlaceThickness = 3;
         public static Color PlaceColor = Color.Black;
 
         // Transition parameters
         public static uint TransitionWidth = 10;
-        public static uint TransitionHeight = 80;
+        public static uint TransitionHeight = 50;
         public static Color TransitionColor = Color.Black;
 
         // Token parameters
-        public static uint TokenWidth = 15;
-        public static uint TokenHeight = 15;
+        public static uint TokenWidth = 10;
+        public static uint TokenHeight = 10;
         public static Color TokenColor = Color.Black;
+
+        // Line parameters
+        public static uint LineThickness = 3;
+        public static Color LineColor = Color.Black;
     }
 }
--- LabWork/Domain/FigureParameters.cs
+++ LabWork/Domain/FigureParameters.cs
@@ -4,6 +4,7 @@
     {
         public uint Width;
         public uint Height;
+        public uint Thickness;
         public Color Color;
     }
 }
