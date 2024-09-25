be6dcadf53edc9b60c0503cdbbf29352303eb539
Ilya Pupyshev <pupischev.ilja@gmail.com>
2024-09-26 00:34:57 +0300

    little changes for FigureParameters

--- LabWork/Domain/FigureParameters.cs
+++ LabWork/Domain/FigureParameters.cs
@@ -3,7 +3,9 @@
     public struct FigureParameters
     {
         public Size ShapeMetrics { get; set; }
+
         public Color BrushColor { get; set; }
+
         public uint Thickness { get; set; }
     }
 }
