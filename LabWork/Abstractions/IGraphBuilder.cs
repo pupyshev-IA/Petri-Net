88800852c6c61d531b432a1811c6c4441e806b6e
Ilya Pupyshev <pupischev.ilja@gmail.com>
2024-09-26 16:42:54 +0300

    Added abstraction for GraphBuilder, updated GraphInfo

---
+++ LabWork/Abstractions/IGraphBuilder.cs
@@ -0,0 +1,12 @@
+﻿using LabWork.Domain;
+using LabWork.Domain.GraphElements;
+
+namespace LabWork.Abstractions
+{
+    public interface IGraphBuilder
+    {
+        GraphInfo BuildPetriGraph(ScrollableControl layout, List<int> tokenSequence);
+
+        void VisualizePetriGraph(GraphInfo graphInfo, ScrollableControl layout, Graphics graphics);
+    }
+}
--- LabWork/Domain/GraphInfo.cs
+++ LabWork/Domain/GraphInfo.cs
@@ -4,10 +4,10 @@ namespace LabWork.Domain
 {
     public class GraphInfo
     {
-        public IDictionary<int, Place> PlacesInfo { get; set; }
+        public IDictionary<int, Place> PlacesInfo { get; set; } = new Dictionary<int, Place>();
 
-        public IDictionary<int, Transition> TransitionsInfo { get; set; }
+        public IDictionary<int, Transition> TransitionsInfo { get; set; } = new Dictionary<int, Transition>();
 
-        public IDictionary<int, Token> TokensInfo { get; set; }
+        public IDictionary<int, Token> TokensInfo { get; set; } = new Dictionary<int, Token>();
     }
 }
